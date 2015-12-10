386e66e755fcb06601f7f09aa666f85690e00569
Adam Lopez <Adamf04@gmail.com>
2015-12-10 16:11:10 -0500

    Crud works correctly for posts.

--- app/controllers/forum_posts_controller.rb
+++ app/controllers/forum_posts_controller.rb
@@ -1,7 +1,7 @@
 class ForumPostsController < ApplicationController
 
 	def index
-		forum_id = params[:id]
+		forum_id = params[:forum_thread_id]
 
 		@forum_list = ForumThread.find_by(id: forum_id)
 
@@ -34,9 +34,57 @@ class ForumPostsController < ApplicationController
 		end
 	end
 
+	def edit
+		forum_id = params[:forum_thread_id]
+		post_id = params[:id]
+
+		@forum_list = ForumThread.find_by(id: forum_id)
+			if @forum_list == nil 
+				redirect_to('/404')
+			else
+				@forum_post = @forum_list.forum_posts.find_by(id: post_id)
+					if @forum_post == nil
+						redirect_to('/404')
+					else
+						render("edit")
+					end
+			end
+	end
+
+	def update
+		forum_id = params[:forum_thread_id]
+		post_id = params[:id]
+
+		@forum_list = ForumThread.find_by(id: forum_id)
+			if @forum_list == nil
+				redirect_to("/404")
+			else
+				@forum_post = @forum_list.forum_posts.find_by(id: post_id)
+				if @forum_post.update(post_params)
+					redirect_to("/forum_threads/#{@forum_list.id}")
+				else
+					render('edit')
+				end
+			end
+	end
+
+	def destroy
+		@forum_post = ForumPost.find_by(id: params[:id])
+		if @forum_post == nil
+			redirect_to("/404")
+		else
+			@forum_post.destroy
+
+			flash[:notice] = "Post was successfully deleted"
+
+			forum_id = params[:forum_thread_id]
+			redirect_to("/forum_threads/#{forum_id}")
+		end
+	end 
+
 private
 
-		def post_params
-			params.require(:forum_post).permit(:body)
-		end
+	def post_params
+		params.require(:forum_post).permit(:body)
+	end
 end
